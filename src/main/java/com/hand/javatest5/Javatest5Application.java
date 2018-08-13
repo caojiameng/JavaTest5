5a3dc56313dcb88cbbcb484fddabd53e026bd092
caojiameng <jiameng.cao@hand-china.com>
2018-08-13 14:08:05 +0800

    spring框架

---
+++ src/main/java/com/hand/javatest5/Javatest5Application.java
@@ -0,0 +1,12 @@
+package com.hand.javatest5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javatest5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javatest5Application.class, args);
+    }
+}
